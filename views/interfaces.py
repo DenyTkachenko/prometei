cdff9b55d1680090109638dcfabe80e1db5a6988
Denys Tkachenko <denystka.edu@gmail.com>
2025-07-18 13:41:37 +0300

    add first implementation

--- views/interfaces.py
+++ views/interfaces.py
@@ -1,12 +1,13 @@
 from abc import ABC, abstractmethod
+from typing import Tuple
 
 class BaseInterface(ABC):
     @abstractmethod
-    def send_message(self, chat_id: int, text: str):
+    def send_message(self, chat_id: str, text: str) -> None:
         pass
 
     @abstractmethod
-    def receive_message(self):
+    def receive_message(self, prompt: str = "") -> Tuple[str, str]:
         """Blocking call or event-driven, depends on implementation."""
         pass
 
