eca368425f62c8e4595044399ee4570bb9f7eadd
Олексій Ієвенко <igrok1908@gmail.com>
2025-07-19 12:17:29 +0300

    The object PromptSession was injected in CliInterface and prompt_session.prompt in receive_message return customer input

--- views/cli_interface.py
+++ views/cli_interface.py
@@ -1,10 +1,15 @@
+from prompt_toolkit import PromptSession
+
 from views.interfaces import BaseInterface
 
 class CLIInterface(BaseInterface):
     """Concrete Interface for command‑line interaction."""
+    def __init__(self, prompt_session: PromptSession):
+        self.prompt_session = prompt_session
+
     def send_message(self, user_id, text: str) -> None:
         print(text)
 
     def receive_message(self, user_id, prompt: str = "") -> str:
         # Prompt on the same line and return what the user typed
-        return input(prompt)
+        return self.prompt_session.prompt(prompt)
