809ceb7d0ea03a87057f05f7b745ddc855ffcf0f
Олексій Ієвенко <igrok1908@gmail.com>
2025-07-20 16:09:26 +0300

    deleted comment from views completer prompt_session

--- views/completer/prompt_session.py
+++ views/completer/prompt_session.py
@@ -2,15 +2,6 @@ from prompt_toolkit import PromptSession
 
 from views.completer.command_completer import CommandCompleter
 
-"""
-Функція get_prompt_session приймає список комманд
-та повертає об'єкт типу PromptSession
-
-Аргументи:
-commands: list[str] - список комманд, що повинні підказуватися користувачу
-
-return: PromptSession - об'єкт типу PromptSession
-"""
 
 def get_prompt_session(commands: list[str]) -> PromptSession:
     prompt_session = None
